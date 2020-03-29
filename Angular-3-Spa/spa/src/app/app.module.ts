2796940c394d61fa6cd2fac4b6c0788ee9a47472
AgustinArielGalarza <agustin.galarza96@gmail.com>
2020-03-29 19:16:19 -0300

    Routes Pagina de Heroes

---
+++ Angular-3-Spa/spa/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+//Rutas
+import { RouteModule } from './app.routes'
+
+//Servicios
+import { HeroesService} from './servicios/heroe.service';
+
+//Componentes
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/share/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { FormsModule } from '@angular/forms'; 
+import {APP_BASE_HREF} from '@angular/common';
+import { HeroeComponent } from './components/heroe/heroe.component'; 
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+    HeroeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouteModule
+  ],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: '/'},
+    HeroesService
+  ],
+  bootstrap: [AppComponent],
+  
+})
+export class AppModule { }
